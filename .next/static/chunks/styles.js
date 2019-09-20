d54a294d3200092c3172cefe3e66eec98ed9902e
d8musica <d8musica@gmail.com>
2019-09-20 14:42:29 -0500

    underconstruction1

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -103,7 +103,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1569007625481");
+            injectCss(link, link.href.split("?")[0] + "?unix=1569008513305");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e0ddbd2e307551bce9c8";
+/******/ 	var hotCurrentHash = "53e73b5bc24e5eda5dbb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
